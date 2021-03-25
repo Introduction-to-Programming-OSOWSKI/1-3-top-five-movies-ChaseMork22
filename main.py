87b9af95e7b20c9234a274f1defd87dd6dca100e
Mork Chase <817251@stma.k12.mn.us>
2021-03-25 10:02:14 -0500

    topFiveMovies4

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def topFiveMovies(movie1, movie2, movie3, movie4, movie5):
-    print(movie1 + movie2 + movie3 + movie4 + movie5)
-topFiveMovies("Speed Racer ", "Good Burger ", "Water Boy ", "Cars ", "Cars 2")
+    print(movie1 + " " + movie2 + " " + movie3 + " " + movie4 + " " + movie5)
+topFiveMovies("Speed Racer", "Good Burger", "Water Boy", "Cars", "Cars 2")
